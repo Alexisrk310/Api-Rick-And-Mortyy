3a20f8f728b7bfe3b9d0c4774390e02a9a5918e4
dylanbatar-co <75990614+dylanbatar-co@users.noreply.github.com>
2021-01-05 14:14:33 -0500

    Renombra las variables y formatea un poco el codigo
    Los `For` para este caso no son recomendados porque tu no sabes cuantos datos te van a venir del endPoint

--- app.js
+++ app.js
@@ -1,47 +1,28 @@
 
-const rick= fetch('https://rickandmortyapi.com/api/character')
-    rick
-        .then((res)=>res.json())
-        .then((data)=>{console.log(data)
-          let content1 =  document.querySelector('.content1')
-          
-  for (let i = 0; i<20; i++) {
-    let codigo = `
-    <div class="content2">
-    
-    <img src="${data.results[i].image}" class="img">
-            <div class="content">
-            
-    
-                <div class="flex-content">
-                    <div>
-    
-                        <label> <b>Nombre:</b> ${data.results[i].name}</label> <br>     
-                        <label > <b>Especie:</b> ${data.results[i].species}</label> 
-                        <label> <b>Sexo: </b> ${data.results[i].gender}</label> 
-    
-    
-                    </div>
-    
-                    <div>
-                        <label> <b>Estado: </b> ${data.results[i].status}</label> <br>
-                        <label> <b>Tipo:</b> ${data.results[i].type}</label>
-                    </div>
-    
-    
-    
-                </div>
-            </div>
-    
-     </div>`
-     content1.innerHTML+= codigo
-
- }
-
-  
-            console.log(data.results[0].name)
-
+const response = fetch('https://rickandmortyapi.com/api/character')
+    response
+        .then((res)=> res.json())
+        .then((data)=> {
+            let container =  document.querySelector('.content1')
+            data.results.forEach((character) => {
+                let card = `
+                <div class="content2">
+                <img src="${character.image}" class="img">
+                        <div class="content">
+                            <div class="flex-content">
+                                <div>   
+                                    <label> <b>Nombre:</b> ${character.name}</label> <br>     
+                                    <label > <b>Especie:</b> ${character.species}</label> 
+                                    <label> <b>Sexo: </b> ${character.gender}</label>  
+                                </div> 
+                                <div>
+                                    <label> <b>Estado: </b> ${character.status}</label> <br>
+                                    <label> <b>Tipo:</b> ${character.type}</label>
+                                </div>
+                            </div>
+                        </div>  
+                 </div>`
+                container.innerHTML+= card
+             }  )
         })
         .catch((e)=>console.log(e))
-        
-
